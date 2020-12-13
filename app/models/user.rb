5cac78e727b16750981d32a3cba64acfbf4ddbc7
taj1-takum1 <takumin390511@gmail.com>
2020-12-13 20:06:38 +0900

    バリデーションまとめ

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,17 @@ class User < ApplicationRecord
   has_many :purchase_records
   with_options presence: true do
     validates :nickname
-    validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Input half-width alphanumeric characters."}
-    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "is invalid. Input full-width characters."}
-    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "is invalid. Input full-width characters."}
-    validates :last_furigana, format: {with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width characters."}
-    validates :first_furigana, format: {with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width characters."}
     validates :birthday
+    validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Input half-width alphanumeric characters."}
+
+    with_options format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "is invalid. Input full-width characters."} do
+      validates :last_name
+      validates :first_name
+    end
+    
+    with_options format: {with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width characters."} do
+      validates :last_furigana
+      validates :first_furigana
+    end
   end
 end
