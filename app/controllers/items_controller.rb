91a05b3e37fce13f1ea148cf1cd0305b92ffee42
taj1-takum1 <takumin390511@gmail.com>
2020-12-15 15:54:33 +0900

    updateアクション修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,8 +36,7 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item.update(item_params)
-    if @item.valid?
+    if @item.update(item_params)
       redirect_to item_path(params[:id])
     else
       render :edit
